e5eeb928fe75dd6940012cee54a6f79d38e73dc7
bugong <1071300360@qq.com>
2015-09-02 17:25:20 +0800

    更新webpack配置文件，经测试可行

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,26 +2,31 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+console.log(__dirname);
+
 module.exports = {
 	entry: {
-		page1: path.join(__dirname, 'src/js/entry_1.js'),
+		page1: path.join(__dirname, 'src/js/entry_1.js'),  //入口文件
 		page2: path.join(__dirname, 'src/js/entry_2.js')
 	},
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].js',
-		publicPath: './dist/'
+		path: path.join(__dirname, 'out'),  //打包输出的路径
+		filename: '[name].js',  //打包后的文件名
+		publicPath: './out/'  //html引用的资源，打包后的前缀路径
 	},
 	module: {
 		loaders: [
 			{test: /\.js$/, loader: 'babel'},
-			{test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
 			{test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'},
-			{test: /\.scss$/, loader: 'style!css!sass'}
+			{test: /\.css$/, loader: 'style!css'}, 
+			//{test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},  //以css为后缀的文件，用ExtractTextPlugin插件提取出来,不能简写
+			//{test: /\.scss$/, loader: 'style!css!sass'}
+			{test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}  //以scss为后缀的文件，用ExtractTextPlugin插件提取出来
+			
 		]
 	},
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin('common.js'),
-		new ExtractTextPlugin('style.css', {allChunks: true})
+		new ExtractTextPlugin('ceshi.css', {allChunks: true})  //不自动向页面插入link
 	]
 };
