834d128822f40244e26d3fa5054061df45028c94
yinyanlv <yanlv.yin@gmail.com>
2016-07-21 13:53:38 +0800

    修改gulp和webpack笔记

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,33 +1,33 @@
-//引入插件
-var gulp = require('gulp'),  
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
+// 引入插件
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  livereload = require('gulp-livereload');
 
 var pathMap = {
-	cssSrcPath: 'src/css',
-	cssDistPath: 'dist/css',
-	jsSrcPath: 'src/js/*.js',
-	jsDistPath: 'dist/js',
-	imgSrcPath: 'src/img/*',
-	imgDistPath: 'dist/img'
+  cssSrcPath: 'src/css',
+  cssDistPath: 'dist/css',
+  jsSrcPath: 'src/js/*.js',
+  jsDistPath: 'dist/js',
+  imgSrcPath: 'src/img/*',
+  imgDistPath: 'dist/img'
 };
 
-var concatJsName = 'app.js';  //配置合并js操作生成的目标文件名，.js不可省略
+var concatJsName = 'app.js';  // 配置合并js操作生成的目标文件名，.js不可省略
 
-//样式
-gulp.task('css', function() {  
+// 样式
+gulp.task('css', function () {
   return sass(pathMap.cssSrcPath, {style: 'expanded'})
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  // .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(pathMap.cssDistPath))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
@@ -35,12 +35,12 @@ gulp.task('css', function() {
     .pipe(notify({message: 'css task complete'}));
 });
 
-//脚本
-gulp.task('js', function() {  
+// 脚本
+gulp.task('js', function () {
   return gulp.src(pathMap.jsSrcPath)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
-    .pipe(concat(concatJsName))  //如果不需要合并js，则删除之
+    .pipe(concat(concatJsName))  // 如果不需要合并js，则删除之
     .pipe(gulp.dest(pathMap.jsDistPath))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
@@ -48,37 +48,37 @@ gulp.task('js', function() {
     .pipe(notify({message: 'js task complete'}));
 });
 
-//图片
-gulp.task('img', function() {  
+// 图片
+gulp.task('img', function () {
   return gulp.src(pathMap.imgSrcPath)
     .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
     .pipe(gulp.dest(pathMap.imgDistPath))
     .pipe(notify({message: 'img task complete'}));
 });
 
-//清理
-gulp.task('clean', function() {  
+// 清理
+gulp.task('clean', function () {
   return gulp.src([pathMap.cssDistPath, pathMap.jsDistPath, pathMap.imgDistPath], {read: false})
     .pipe(clean());
 });
 
-//默认任务
-gulp.task('default', ['clean'], function() {  
-    gulp.start('css', 'js', 'img');
+// 默认任务
+gulp.task('default', ['clean'], function () {
+  gulp.start('css', 'js', 'img');
 });
 
-//监听
-gulp.task('watch', function() {
-  //建立实时重启服务器
+// 监听
+gulp.task('watch', function () {
+  // 建立实时重启服务器
   var server = livereload();
-  //监听scss档
+  // 监听scss档
   gulp.watch(pathMap.cssSrcPath, ['css']);
-  //监听js档
+  // 监听js档
   gulp.watch(pathMap.jsSrcPath, ['js']);
-  //监听图片
+  // 监听图片
   gulp.watch(pathMap.imgSrcPath, ['img']);
-  //监听所有在dist目录下的文件
-  gulp.watch([pathMap.cssDistPath, pathMap.jsDistPath, pathMap.imgDistPath]).on('change', function(file) {
+  // 监听所有在dist目录下的文件
+  gulp.watch([pathMap.cssDistPath, pathMap.jsDistPath, pathMap.imgDistPath]).on('change', function (file) {
     server.changed(file.path);
   });
 });
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,28 +3,28 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: {
-		page1: path.join(__dirname, 'src/js/entry_1.js'),  //入口文件
-		page2: path.join(__dirname, 'src/js/entry_2.js')
-	},
-	output: {
-		path: path.join(__dirname, 'out'),  //打包输出的路径
-		filename: '[name].js',  //打包后的文件名
-		publicPath: './out/'  //html引用的资源，打包后的前缀路径
-	},
-	module: {
-		loaders: [
-			{test: /\.js$/, loader: 'babel'},
-			{test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'},
-			{test: /\.css$/, loader: 'style!css'},
-			//{test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},  //以css为后缀的文件，用ExtractTextPlugin插件提取出来,不能简写
-			//{test: /\.scss$/, loader: 'style!css!sass'}
-			{test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}  //以scss为后缀的文件，用ExtractTextPlugin插件提取出来
+  entry: {
+    page1: path.join(__dirname, 'src/js/entry_1.js'),  // 入口文件
+    page2: path.join(__dirname, 'src/js/entry_2.js')
+  },
+  output: {
+    path: path.join(__dirname, 'out'),  // 打包输出的路径
+    filename: '[name].js',  // 打包后的文件名
+    publicPath: './out/'  // html引用的资源，打包后的前缀路径
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel'},
+      {test: /\.(png|jpg|gif)$/, loader: 'url?limit=8192'},
+      {test: /\.css$/, loader: 'style!css'},
+      // {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},  // 以css为后缀的文件，用ExtractTextPlugin插件提取出来,不能简写
+      // {test: /\.scss$/, loader: 'style!css!sass'}
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')}  // 以scss为后缀的文件，用ExtractTextPlugin插件提取出来
 
-		]
-	},
-	plugins: [
-		new webpack.optimize.CommonsChunkPlugin('common.js'),
-		new ExtractTextPlugin('ceshi.css', {allChunks: true})  //不自动向页面插入link
-	]
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new ExtractTextPlugin('ceshi.css', {allChunks: true})  // 不自动向页面插入link
+  ]
 };
